74c2a14bc7b29c1734ac2f444146ccfcd699a716
Shawn M Moore <code@sartak.org>
2013-06-28 20:27:17 -0400

    Remove Demo comments

--- lib/TAEB/AI/Magus.pm
+++ lib/TAEB/AI/Magus.pm
@@ -27,30 +27,22 @@ my @behaviors = (qw/
     search
 /);
 
-# The framework calls this method on the AI object to determine what action to
-# do next. An action is an instance of TAEB::Action, which is basically a handy
-# object wrapper around a NetHack command like "s" for search.
 sub next_action {
     my $self = shift;
 
     my @methods = __PACKAGE__->meta->get_all_method_names;
 
-    # Try each of these behaviors (which are methods) in order...
     for my $behavior (@behaviors) {
         for my $method (grep { /^$behavior$/ } @methods) {
             my $action = $self->$method
                 or next;
 
-            # "currently" is for reporting what we're doing on the second-to-last
-            # line of the TAEB display. Optional but you should set it anyway.
             $self->currently($behavior);
 
             return $action;
         }
     }
 
-    # We must be trapped! Search for a secret door. This is a nice fallback
-    # since you can search indefinitely.
     $self->currently('to_search');
     return $self->to_search;
 }
@@ -138,10 +130,8 @@ sub buff_great_potion {
 }
 
 sub pray {
-    # This returns false if we prayed recently, or our god is angry, etc.
     return unless TAEB::Action::Pray->is_advisable;
 
-    # Only pray if we're low on nutrition or health.
     return unless TAEB->nutrition < 0
                || TAEB->in_pray_heal_range;
 
@@ -167,7 +157,6 @@ sub bolt {
     );
 }
 
-# Find an adjacent enemy and swing at it.
 sub melee {
     if_adjacent(
         sub {
@@ -177,7 +166,6 @@ sub melee {
     );
 }
 
-# Find an enemy on the level and hunt it down.
 sub hunt {
     path_to(sub {
         my $tile = shift;
@@ -188,7 +176,6 @@ sub hunt {
     }, include_endpoints => 1);
 }
 
-# If we're on stairs then descend.
 sub descend {
     return unless TAEB->current_tile->type eq 'stairsdown';
 
@@ -200,13 +187,10 @@ sub to_item {
     path_to(sub { shift->is_interesting });
 }
 
-# If we see stairs, then go to them.
 sub to_stairs {
     path_to('stairsdown');
 }
 
-# If there's an adjacent closed door, try opening it. If it's locked, kick it
-# down.
 sub open_door {
     if_adjacent(closeddoor => sub {
         return 'kick' if shift->is_locked;
@@ -214,17 +198,14 @@ sub open_door {
     });
 }
 
-# If we see a closed door, then go to it.
 sub to_door {
     path_to('closeddoor', include_endpoints => 1);
 }
 
-# If there's an unexplored tile (tracked by the framework), go to it.
 sub explore {
     path_to(sub { shift->unexplored });
 }
 
-# If there's an unsearched tile next to us, search.
 sub search {
     if_adjacent(
         sub { $_[0]->is_searchable && $_[0]->searched < 30 },
@@ -232,7 +213,6 @@ sub search {
     );
 }
 
-# If there's an unsearched tile, go to it.
 sub to_search {
     path_to(
         sub { $_[0]->is_searchable && $_[0]->searched < 30 },
@@ -240,12 +220,6 @@ sub to_search {
     );
 }
 
-# These helper functions make our behavior code far more concise and
-# declarative.
-
-# find_adjacent finds and adjacent tile that satisfies some predicate. It takes
-# a coderef and returns the (tile, direction) corresponding to the adjacent
-# tile that returned true for the predicate.
 sub find_adjacent {
     my $code = shift;
 
@@ -258,15 +232,10 @@ sub find_adjacent {
     return wantarray ? ($tile, $direction) : $tile;
 }
 
-# if_adjacent takes a predicate and an action name. If the predicate returns
-# true for any of the adjacent tiles, then the action will be instantiated and
-# returned.
 sub if_adjacent {
     my $code   = shift;
     my $action = shift;
 
-    # Allow caller to pass in a tile type name to check for an adjacent tile
-    # with that type.
     if (!ref($code)) {
         my $type = $code;
         $code = sub { shift->type eq $type };
@@ -275,15 +244,10 @@ sub if_adjacent {
     my ($tile, $direction) = find_adjacent($code);
     return if !$tile;
 
-    # If they pass in a coderef for action, then they need to do some additional
-    # processing based on tile type. Let them decide an action name.
     $action = $action->($tile, $direction) if ref($action);
 
     my $action_class = "TAEB::Action::\u$action";
 
-    # We only want to pass in a direction if the action cares about direction.
-    # Actions that care about direction do the TAEB::Action::Role::Direction
-    # "role". Kind of like a Java interface, but more awesome.
     my %args;
     $args{direction} = $direction
         if $action_class->does('TAEB::Action::Role::Direction');
@@ -291,21 +255,14 @@ sub if_adjacent {
     return $action_class->new(%args);
 }
 
-# path_to takes a predicate (and optional arguments to pass to the pathfinder)
-# and finds the closest tile that satisfies that predicate. If there is such a
-# tile, then a Path will be returned.
-# If you need to find a path adjacent to an unwalkable tile, then pass in
-# include_endpoints => 1.
 sub path_to {
     my $code = shift;
 
-    # Allow caller to pass in a tile type name to find a tile with that type.
     if (!ref($code)) {
         my $type = $code;
         $code = sub { shift->type eq $type };
     }
 
-    # TAEB will inflate a path into a Move action for us
     return TAEB::World::Path->first_match($code, @_);
 }
 
