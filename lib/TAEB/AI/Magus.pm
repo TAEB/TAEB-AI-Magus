b2445c8687adcb2e984e5253c228db3c27a0dd23
Shawn M Moore <code@sartak.org>
2013-07-05 16:48:58 -0400

    Ditto goodies

--- lib/TAEB/AI/Magus.pm
+++ lib/TAEB/AI/Magus.pm
@@ -767,6 +767,8 @@ sub want_goody {
     my $self = shift;
     my $item = shift;
 
+    return if $item->cost;
+
     return 1 if $item->match('Magicbane');
     return 1 if $item->type eq 'spellbook';
     return 1 if $item->match('magic marker');
