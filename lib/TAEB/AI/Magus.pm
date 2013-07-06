467aea54b1219b1c2d26e613dd7a737af8e992e6
Shawn M Moore <code@sartak.org>
2013-07-05 22:23:30 -0400

    nutrition doesn't include quantity

--- lib/TAEB/AI/Magus.pm
+++ lib/TAEB/AI/Magus.pm
@@ -1380,7 +1380,7 @@ sub attack_spell {
 }
 
 sub carried_nutrition {
-    (sum map { $_->nutrition } TAEB->inventory->find(type => "food")) || 0;
+    (sum map { $_->quantity * $_->nutrition } TAEB->inventory->find(type => "food")) || 0;
 }
 
 subscribe query_pickupitems => sub {
